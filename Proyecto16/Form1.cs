4ef5fd7a4072e15aa8f9cf817777eba3bf46bf37
Zarterken <albinuribe1@gmail.com>
2025-11-21 01:29:55 -0400

    Proyectos

---
+++ Proyecto16/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void image_UploadBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.image_UploadBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.imageUploadDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+ 
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFd = new OpenFileDialog();
+            openFd.Filter = "Image only | *.jpg; *.jpeg; *.png; *.gif; *.bmp";
+
+            DialogResult dr = openFd.ShowDialog();
+
+            imgPictureBox.Image = Image.FromFile(openFd.FileName);
+
+            imgpathLabel1.Text = openFd.FileName;
+        }
+    }
+}
